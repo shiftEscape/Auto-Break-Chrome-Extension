5be2b25f9864143c3b61dd62116a8d7c7680e442
Alvin James Bellero <ajames.bellero@gmail.com>
2016-07-19 18:11:55 +0800

    Updated process algorithm of plugin

--- popup.js
+++ popup.js
@@ -1,85 +1,106 @@
 
-var breakInUrl = 'http://google.com';
-var breakOutUrl = 'http://google.com';
+(function() {
 
-function generateTimes (selElem, start, limit, step, text) {
-	selElem.innerHTML = "";
-	for(var i=start; i<=limit; i+=step) {
-		var option = document.createElement("option");
-		var txt = (i < 10?'0'+i:i);
-		option.innerHTML = txt + text; option.value = i;
-		selElem.appendChild(option);
+	'use strict';
+
+	var tInt = null;
+	var WATCHER_KEY = 'watcherStatus';
+	var breakInUrl = 'http://google.com';
+	var breakOutUrl = 'http://google.com';
+
+	function generateTimes (selElem, start, limit, step, text) {
+		selElem.innerHTML = "";
+		for(var i=start; i<=limit; i+=step) {
+			var option = document.createElement("option");
+			var txt = (i < 10?'0'+i:i);
+			option.innerHTML = txt + text; option.value = i;
+			selElem.appendChild(option);
+		}
 	}
-}
 
-function getStrTime (hr, min) {
-	var d = new Date();
-	d.setHours(hr, min, 0);
-	return d.getTime();
-}
+	function getStrTime (hr, min) {
+		var d = new Date();
+		d.setHours(hr, min, 0);
+		return d.getTime();
+	}
 
-function getCurrentTime () {
-	var d = new Date();
-	return d.getTime();
-}
+	function getCurrentTime () {
+		var d = new Date();
+		return d.getTime();
+	}
 
-function createFormElement (_id, _action) {
-	var frm = document.createElement('form');
-	frm.id = _id; frm.action = _action; frm.method = 'post';
-	return frm;
-}
+	function watchBreakTime () {
+		tInt = setInterval(function() {
+			if( isWatcherEnabled() ) {
+				console.log(getCurrentTime());
+				var time = (getStrTime(17, 58) == getCurrentTime());
+				if (time) enableWatcher(false);
+			}
+		}, 1000)
+	}
 
-function watchBreakTime () {
-	var tInt = setInterval(function() {
-		console.log(getCurrentTime());
-		var time = (getStrTime(11, 34) == getCurrentTime());
-		if (time) {
-			console.info('TIME!!');
+	function enableWatcher (bool) {
+		localStorage.setItem(WATCHER_KEY, (bool?'ENABLED':'DISABLED'));
+		if (bool) {
+			console.warn('STARTED!')
+			watchBreakTime();
+		} else {
+			console.warn('STOPPED!')
+			setButtonWatcherStatus();
 			clearInterval(tInt);
 		}
-	}, 1000)
-}
+	}
 
-function sendRequest (url, handleStateChange) {
-	var xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = handleStateChange;
-	xhr.open("POST", url, true);
-	xhr.send();
-}
+	function isWatcherEnabled () {
+		var stat = localStorage.getItem(WATCHER_KEY);
+		return (stat === 'ENABLED' ? true : false);
+	}
 
-// DOM onload
-document.addEventListener('DOMContentLoaded', function() {
+	function setButtonWatcherStatus () {
+		var isEnabled = isWatcherEnabled(),
+				enableAutoBreakButton  = document.getElementById('enableAutoBreak'),
+				disableAutoBreakButton = document.getElementById('disableAutoBreak');
+		enableAutoBreakButton.style.display = (isEnabled? 'none':'block');
+		disableAutoBreakButton.style.display = (isEnabled? 'block':'none');
+	}
 
-  var enableAutoBreakButton = document.getElementById('enableAutoBreak');
-  var disableAutoBreakButton = document.getElementById('disableAutoBreak');
+	function sendRequest (url, handleStateChange) {
+		var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = handleStateChange;
+		xhr.open("POST", url, true);
+		xhr.send();
+	}
 
-  var selectSTHour = document.getElementById('st-hour');
-  var selectSTMin  = document.getElementById('st-min');
+	// DOM onload
+	document.addEventListener('DOMContentLoaded', function() {
 
-	var selectDuration = document.getElementById('duration');
+		setButtonWatcherStatus();
 
-	// Load all times
-	generateTimes(selectSTHour, 0, 23, 1, '');
-	generateTimes(selectSTMin, 0, 59, 1, '');
-	generateTimes(selectDuration, 10, 60, 10, ' mins');
+	  var enableAutoBreakButton = document.getElementById('enableAutoBreak');
+	  var disableAutoBreakButton = document.getElementById('disableAutoBreak');
 
-	// Enable button
-  enableAutoBreakButton.addEventListener('click', function() {
-		var thisBtn = this;
-		var cb = function () {
-			thisBtn.style.display = 'none';
-			disableAutoBreakButton.style.display = 'block';
-		}; sendRequest('http://www.google.com', cb);
-  }, false);
+	  var selectSTHour = document.getElementById('st-hour');
+	  var selectSTMin  = document.getElementById('st-min');
 
-	// Disable button
-	disableAutoBreakButton.addEventListener('click', function() {
-		var thisBtn = this;
-		var cb = function () {
-			thisBtn.style.display = 'none';
-			enableAutoBreakButton.style.display = 'block';
-		}; sendRequest('http://www.google.com', cb);
+		var selectDuration = document.getElementById('duration');
+
+		// Load all times
+		generateTimes(selectSTHour, 0, 23, 1, '');
+		generateTimes(selectSTMin, 0, 59, 1, '');
+		generateTimes(selectDuration, 10, 60, 10, ' mins');
+
+		// Enable button
+	  enableAutoBreakButton.addEventListener('click', function() {
+			enableWatcher(true);
+			setButtonWatcherStatus();
+	  }, false);
+
+		// Disable button
+		disableAutoBreakButton.addEventListener('click', function() {
+			enableWatcher(false);
+			setButtonWatcherStatus();
+		}, false);
 
 	}, false);
 
-}, false);
+}).call(this);
