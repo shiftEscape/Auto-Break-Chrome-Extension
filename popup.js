d03d9ba93e61e7628056e8e81f7716e6139bf8ff
Alvin James Bellero <alvinb@zylun.com>
2016-07-19 14:44:34 +0800

    Add temp changes for editing

--- popup.js
+++ popup.js
@@ -1,10 +1,13 @@
 
-function generateTimes (selElem, limit) {
+var breakInUrl = 'http://google.com';
+var breakOutUrl = 'http://google.com';
+
+function generateTimes (selElem, start, limit, step, text) {
 	selElem.innerHTML = "";
-	for(var i=0; i<=limit; i++) {
+	for(var i=start; i<=limit; i+=step) {
 		var option = document.createElement("option");
-		i = (i < 10?'0'+i:i);
-		option.innerHTML = i; option.value = i;
+		var txt = (i < 10?'0'+i:i);
+		option.innerHTML = txt + text; option.value = i;
 		selElem.appendChild(option);
 	}
 }
@@ -20,59 +23,63 @@ function getCurrentTime () {
 	return d.getTime();
 }
 
+function createFormElement (_id, _action) {
+	var frm = document.createElement('form');
+	frm.id = _id; frm.action = _action; frm.method = 'post';
+	return frm;
+}
 
+function watchBreakTime () {
+	var tInt = setInterval(function() {
+		console.log(getCurrentTime());
+		var time = (getStrTime(11, 34) == getCurrentTime());
+		if (time) {
+			console.info('TIME!!');
+			clearInterval(tInt);
+		}
+	}, 1000)
+}
+
+function sendRequest (url, handleStateChange) {
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = handleStateChange;
+	xhr.open("POST", url, true);
+	xhr.send();
+}
+
+// DOM onload
 document.addEventListener('DOMContentLoaded', function() {
+
   var enableAutoBreakButton = document.getElementById('enableAutoBreak');
   var disableAutoBreakButton = document.getElementById('disableAutoBreak');
 
   var selectSTHour = document.getElementById('st-hour');
   var selectSTMin  = document.getElementById('st-min');
 
-	var selectENHour = document.getElementById('end-hour');
-  var selectENMin  = document.getElementById('end-min');
+	var selectDuration = document.getElementById('duration');
 
 	// Load all times
-	generateTimes(selectSTHour, 12);
-	generateTimes(selectSTMin, 60);
-	generateTimes(selectENHour, 12);
-	generateTimes(selectENMin, 60);
+	generateTimes(selectSTHour, 0, 23, 1, '');
+	generateTimes(selectSTMin, 0, 59, 1, '');
+	generateTimes(selectDuration, 10, 60, 10, ' mins');
 
 	// Enable button
   enableAutoBreakButton.addEventListener('click', function() {
-		this.style.display = 'none';
-		disableAutoBreakButton.style.display = 'block';
-
-    chrome.tabs.getSelected(null, function(tab) {
-			var tInt = setInterval(function() {
-				console.log(getCurrentTime());
-				var time = (getStrTime(11, 34) == getCurrentTime());
-				if(time) {
-					console.info('TIME!!');
-					clearInterval(tInt);
-				}
-			}, 1000)
-			// chrome.tabs.sendRequest(tab.id, {action: "getDOM"}, function(response) {
-		  //   console.log(response.dom);
-		  // });
-			// console.log(d.getElementById('lunchIn'));
-			//
-      // var f = d.createElement('form');
-      // f.action = 'http://gtmetrix.com/analyze.html?bm';
-      // f.method = 'post';
-      // var i = d.createElement('input');
-      // i.type = 'hidden';
-      // i.name = 'url';
-      // i.value = tab.url;
-      // f.appendChild(i);
-      // d.body.appendChild(f);
-      // f.submit();
-    });
+		var thisBtn = this;
+		var cb = function () {
+			thisBtn.style.display = 'none';
+			disableAutoBreakButton.style.display = 'block';
+		}; sendRequest('http://www.google.com', cb);
   }, false);
 
 	// Disable button
 	disableAutoBreakButton.addEventListener('click', function() {
-		this.style.display = 'none';
-		enableAutoBreakButton.style.display = 'block';
+		var thisBtn = this;
+		var cb = function () {
+			thisBtn.style.display = 'none';
+			enableAutoBreakButton.style.display = 'block';
+		}; sendRequest('http://www.google.com', cb);
+
 	}, false);
 
 }, false);
