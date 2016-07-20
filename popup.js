b6eb4d2d44e10c9fab7958a5184c24fb713874ac
shiftescape <ajames.bellero@gmail.com>
2016-07-20 15:03:43 +0800

    Updated core files

--- popup.js
+++ popup.js
@@ -3,10 +3,13 @@
 
 	'use strict';
 
-	var tInt = null;
-	var WATCHER_KEY = 'watcherStatus';
-	var breakInUrl = 'http://google.com';
-	var breakOutUrl = 'http://google.com';
+	var port = chrome.runtime.connect({name: "auto-break-port"});
+
+	var WATCHER_KEY = 'watcherStatus',
+			ICON_URL = './AutoBreak48.png';
+
+	var enableAutoBreakButton = null;
+	var disableAutoBreakButton = null;
 
 	function generateTimes (selElem, start, limit, step, text) {
 		selElem.innerHTML = "";
@@ -18,30 +21,32 @@
 		}
 	}
 
-	function getStrTime (hr, min) {
-		var d = new Date();
-		d.setHours(hr, min, 0);
-		return d.getTime();
+	function showNotify (notif) {
+		// Let's check if the browser supports notifications
+		if (!("Notification" in window)) {
+			alert("This browser does not support desktop notification");
+		}
+
+		// Let's check whether notification permissions have already been granted
+		else if (Notification.permission === "granted") {
+			// If it's okay let's create a notification
+			var notification = new Notification('', {body: notif, icon: ICON_URL});
+		}
+
+		// Otherwise, we need to ask the user for permission
+		else if (Notification.permission !== 'denied') {
+			Notification.requestPermission(function (permission) {
+				// If the user accepts, let's create a notification
+				if (permission === "granted") {
+					var notification = new Notification('', {body: notif, icon: ICON_URL});
+				}
+			});
+		}
 	}
 
-	function getCurrentTime () {
-		var d = new Date();
-		return d.getTime();
-	}
-
-	function watchBreakTime () {
-		tInt = setInterval(function() {
-			if( isWatcherEnabled() ) {
-				console.log(getCurrentTime());
-				var time = (getStrTime(17, 58) == getCurrentTime());
-				if (time) enableWatcher(false);
-			}
-		}, 1000)
-	}
-
-	function disableControls (boolDisable) {
+	function disableControls () {
 		var sel = document.getElementsByTagName('select');
-		console.log(sel)
+		var boolDisable = isWatcherEnabled();
 		for(var i=0;i<sel.length;i++) {
 			sel[i].disabled = boolDisable;
 		}
@@ -49,14 +54,7 @@
 
 	function enableWatcher (bool) {
 		localStorage.setItem(WATCHER_KEY, (bool?'ENABLED':'DISABLED'));
-		if (bool) {
-			console.warn('STARTED!')
-			watchBreakTime();
-		} else {
-			console.warn('STOPPED!')
-			setButtonWatcherStatus();
-			clearInterval(tInt);
-		}
+		setButtonWatcherState();
 	}
 
 	function isWatcherEnabled () {
@@ -64,7 +62,7 @@
 		return (stat === 'ENABLED' ? true : false);
 	}
 
-	function setButtonWatcherStatus () {
+	function setButtonWatcherState () {
 		var isEnabled = isWatcherEnabled(),
 				enableAutoBreakButton  = document.getElementById('enableAutoBreak'),
 				disableAutoBreakButton = document.getElementById('disableAutoBreak');
@@ -72,20 +70,46 @@
 		disableAutoBreakButton.style.display = (isEnabled? 'block':'none');
 	}
 
-	function sendRequest (url, handleStateChange) {
-		var xhr = new XMLHttpRequest();
-		xhr.onreadystatechange = handleStateChange;
-		xhr.open("POST", url, true);
-		xhr.send();
+	function setOptionsState () {
+		var opts = localStorage.getItem('setTimes')
+		if(opts !== null) {
+			// window.alert(opts);
+			opts = JSON.parse(opts);
+			document.getElementById('st-hour').value = opts.hr;
+		  document.getElementById('st-min').value = opts.min;
+		  document.getElementById('duration').value = opts.duration;
+		}
+	}
+
+	function getBreakIn () {
+		var tHour = document.getElementById('st-hour');
+		var tMin  = document.getElementById('st-min');
+		return { hr: tHour.value, min: tMin.value };
+	}
+
+	function getBreakOutFromDuration () {
+		var brkIN     = getBreakIn();
+		var dateObj   = new Date();
+		var tDuration = document.getElementById('duration');
+
+		dateObj.setHours(brkIN.hr, brkIN.min, 0);
+		dateObj.setMinutes(dateObj.getMinutes() + parseInt(tDuration.value));
+		return { hr: dateObj.getHours(), min: dateObj.getMinutes() };
+	}
+
+	function storeTimeData (times) {
+		times['duration'] = document.getElementById('duration').value;
+		localStorage.setItem('setTimes', JSON.stringify(times));
 	}
 
 	// DOM onload
 	document.addEventListener('DOMContentLoaded', function() {
 
-		setButtonWatcherStatus();
+		setButtonWatcherState();
+		disableControls();
 
-	  var enableAutoBreakButton = document.getElementById('enableAutoBreak');
-	  var disableAutoBreakButton = document.getElementById('disableAutoBreak');
+	  enableAutoBreakButton = document.getElementById('enableAutoBreak');
+	  disableAutoBreakButton = document.getElementById('disableAutoBreak');
 
 	  var selectSTHour = document.getElementById('st-hour');
 	  var selectSTMin  = document.getElementById('st-min');
@@ -97,18 +121,25 @@
 		generateTimes(selectSTMin, 0, 59, 1, '');
 		generateTimes(selectDuration, 10, 60, 10, ' mins');
 
+		setOptionsState();
+
 		// Enable button
 	  enableAutoBreakButton.addEventListener('click', function() {
+			var brkIN = getBreakIn(),
+					brkOUT = getBreakOutFromDuration();
 			enableWatcher(true);
-			setButtonWatcherStatus();
-			disableControls(true);
-	  }, false);
+			storeTimeData(brkIN);
+			disableControls();
+			port.postMessage({ in: brkIN, out: brkOUT, current: brkIN, type: 'IN' });
+			showNotify('Break watcher started!');
+		}, false);
 
 		// Disable button
 		disableAutoBreakButton.addEventListener('click', function() {
 			enableWatcher(false);
-			setButtonWatcherStatus();
-			disableControls(false);
+			disableControls();
+			showNotify('Break watcher stopped!');
+			port.postMessage({ type: 'STOP' });
 		}, false);
 
 	}, false);
